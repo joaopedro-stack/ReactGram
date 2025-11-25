e195b3a90da3cc420acb2a8a43af3c40aa10ca74
João Pedro <joaopedrobastossantana@gmail.com>
2025-11-25 19:22:17 -0300

    ajustando rotas no front e nome de pasta

--- frontend/src/components/PhotoItem.js
+++ frontend/src/components/PhotoItem.js
@@ -1,19 +1,18 @@
 import './PhotoItem.css'
-
-import { upload } from '../utils/config'
-
 import { Link } from 'react-router-dom'
 
-const PhotoItem = ({photo}) => {
+const PhotoItem = ({ photo }) => {
   return (
     <div className='photo-item'>
-        {photo.image && (
-            <img src={`${upload}/photos/${photo.image}`} alt={photo.title} />
-        )}
-        <h2>{photo.title}</h2>
-        <p className="photo-author">
-            Publicado por: <Link to={`/users/${photo.userId}`}>{photo.userName}</Link>
-        </p>
+      {photo.image && (
+        <img src={photo.image} alt={photo.title} />
+      )}
+
+      <h2>{photo.title}</h2>
+
+      <p className="photo-author">
+        Publicado por: <Link to={`/users/${photo.userId}`}>{photo.userName}</Link>
+      </p>
     </div>
   )
 }
